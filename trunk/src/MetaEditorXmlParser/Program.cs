c797b11aad31dd640f55f770fa48f9a552cd35fd
ylw <ylw@localhost>
2012-07-07 02:04:54 -0500

    fix unfound keywords from documentation and updates api & keyword lists accordingly

--- trunk/src/MetaEditorXmlParser/Program.cs
+++ trunk/src/MetaEditorXmlParser/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using MetaEditorXmlParser.NodeType;
 
 namespace MetaEditorXmlParser
@@ -12,9 +13,14 @@ namespace MetaEditorXmlParser
     {
       var runner = new DataConstructor();
       runner.Analyze();
-      //runner.ProduceChmMaterial("html");
-      foreach (var f in runner.PageNodes.Where(e => e is FunctionNode))
-        Console.WriteLine(((FunctionNode)f).ToSciteApiString());
+        //using (var writer = new XmlTextWriter("output.xml", Encoding.UTF8))
+        //{
+        //    foreach (var e in runner.Root.Children)
+        //        e.Element.WriteTo(writer);
+        //}
+      runner.ProduceChmMaterial("html");
+      //foreach (var f in runner.PageNodes.Where(e => e is FunctionNode))
+      //  Console.WriteLine(((FunctionNode)f).ToSciteApiString());
       //foreach (var s in runner.Constants)
       //    Console.Write(s + " ");
       //Console.ReadLine();
